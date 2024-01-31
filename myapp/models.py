41551f841ee7dc7d79c6211cdfa3bf55cbba9ea0
Jagadeesh Kumar <jagadeeshkumar112311@gmail.com>
2024-01-31 23:35:05 +0530

    j4

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class AnnounceText(models.Model):
-    text=models.CharField(max_length=500)
+    text=models.CharField(max_length=100000)
     # courseoutcome=models.CharField(max_length=100)
     # knowledge_level=models.CharField(max_length=5,default='K1')
     # course_code=models.CharField(max_length=15)
